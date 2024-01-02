35fb86f3c0f10dccd5ca3adb805c3005bbf17e43
Lucas Becerra <lucaseramos13@gmail.com>
2024-01-02 08:10:52 -0300

    my first post request successfully

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,19 +4,19 @@ import { type NextRequest, NextResponse } from 'next/server'
 export async function GET (req: NextRequest) {
   const [users] = await pool.query('SELECT * FROM users')
   console.log(users)
-  return NextResponse.json({ message: 'hola' }, { status: 200 })
+  return NextResponse.json({ message: 'hello' }, { status: 200 })
 }
 export async function POST (req: NextRequest) {
   try {
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    const { user_nick, email_address, first_name, last_name, phonenumber } = await req.json()
+    const { user_nick, email_address, first_name, last_name, phonenumber, password } = await req.json()
 
     const [res] = await pool.query('INSERT INTO users SET ? ', {
-      user_nick, email_address, first_name, last_name, phonenumber
+      user_nick, email_address, first_name, last_name, phonenumber, password
     })
 
     return NextResponse.json({ user_nick, email_address, first_name, last_name, phonenumber, id: res.insertId }, { status: 200 })
-  } catch (error) {
+  } catch (error: any) {
     console.log(error)
     return NextResponse.json({
       message: error.message

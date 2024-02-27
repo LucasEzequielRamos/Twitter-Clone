9b84cb43c9fbf00a731e4bd62fa0afde707a7da5
Lucas Becerra <lucaseramos13@gmail.com>
2024-02-27 02:53:23 -0300

    fix header get problems en getOneUser.ts

--- src/lib/getOneUser.ts
+++ src/lib/getOneUser.ts
@@ -3,12 +3,14 @@ import { headers } from 'next/headers'
 
 export async function getOneUser (id: number): Promise<any> {
   const userData = await getServerSession()
+  const cookie: string | null = headers().get('cookie')
+  const header = new Headers()
+  if (typeof (cookie) === 'string') header.set('Cookie', cookie)
+
   if (userData !== undefined) {
     const res = await fetch(`http://localhost:3000/api/users/${id}`, {
       method: 'GET',
-      headers: {
-        Cookie: headers().get('cookie')
-      }
+      headers: header
     })
 
     const userData = await res.json()
--- src/lib/getUserSession.ts
+++ src/lib/getUserSession.ts
@@ -3,12 +3,13 @@ import { headers } from 'next/headers'
 
 export async function fetchUserInfo (): Promise<any> {
   const userData = await getServerSession()
+  const cookie: string | null = headers().get('cookie')
+  const header = new Headers()
+  if (typeof (cookie) === 'string') header.set('Cookie', cookie)
   if (userData !== undefined) {
     const res = await fetch('http://localhost:3000/api/users', {
       method: 'GET',
-      headers: {
-        Cookie: headers().get('cookie')
-      }
+      headers: header
     })
 
     const userData = await res.json()

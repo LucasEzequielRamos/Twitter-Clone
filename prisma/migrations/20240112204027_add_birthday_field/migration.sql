70e7e7a9de6637efb604aea788246151cc6ed00b
Lucas Becerra <lucaseramos13@gmail.com>
2024-02-27 22:27:25 -0300

    init new db

--- prisma/migrations/20240112204027_add_birthday_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `users` ADD COLUMN `birthday` VARCHAR(255) NULL;
--- prisma/migrations/20240228012530_change/migration.sql
+++ prisma/migrations/20240228012530_change/migration.sql
@@ -3,11 +3,13 @@ CREATE TABLE `Users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_nick` VARCHAR(20) NOT NULL,
     `email_address` VARCHAR(255) NOT NULL,
-    `first_name` VARCHAR(255) NOT NULL,
-    `last_name` VARCHAR(255) NOT NULL,
-    `phonenumber` CHAR(15) NOT NULL,
+    `full_name` VARCHAR(255) NOT NULL,
+    `phonenumber` CHAR(15) NULL,
     `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    `passwords` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NULL,
+    `avatar_url` VARCHAR(255) NULL,
+    `description` VARCHAR(255) NULL,
+    `birthday` VARCHAR(255) NULL,
 
     UNIQUE INDEX `Users_user_nick_key`(`user_nick`),
     UNIQUE INDEX `email`(`email_address`),
@@ -35,6 +37,14 @@ CREATE TABLE `Tweets` (
     PRIMARY KEY (`tweet_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Tweets_likes` (
+    `tweet_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`tweet_id`, `user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Followers` ADD CONSTRAINT `Followers_follower_id_fkey` FOREIGN KEY (`follower_id`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -43,3 +53,9 @@ ALTER TABLE `Followers` ADD CONSTRAINT `Followers_following_id_fkey` FOREIGN KEY
 
 -- AddForeignKey
 ALTER TABLE `Tweets` ADD CONSTRAINT `Tweets_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Tweets_likes` ADD CONSTRAINT `Tweets_likes_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Tweets_likes` ADD CONSTRAINT `Tweets_likes_tweet_id_fkey` FOREIGN KEY (`tweet_id`) REFERENCES `Tweets`(`tweet_id`) ON DELETE RESTRICT ON UPDATE CASCADE;

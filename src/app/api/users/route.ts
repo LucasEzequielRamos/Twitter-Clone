fb0b24b8ea9f55fa320149f96070e1003ed80f9f
Lucas Becerra <lucaseramos13@gmail.com>
2024-02-29 08:24:41 -0300

    console headers

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -9,6 +9,7 @@ import { type UploadApiResponse } from 'cloudinary'
 export async function GET (req: Request) {
   try {
     const session: any = await nextAuthGetServerSession(authOptions)
+    console.log(req.headers)
     console.log(session)
     if (!session) throw new Error('session not found')
 

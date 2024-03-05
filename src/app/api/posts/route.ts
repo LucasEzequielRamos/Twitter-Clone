7683a9c814370bd230f9be40826d021e0340eac8
Lucas Becerra <lucaseramos13@gmail.com>
2024-03-04 21:56:21 -0300

    comment error

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import db from '@/utils/db'
-import { getServerSession } from 'next-auth'
+// import { getServerSession } from 'next-auth'
 
 export async function GET (req: NextRequest) {
   try {
